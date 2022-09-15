5af1309cb546c0c6655df86107cb7535d38fe5a5
Plaian Nicoleta <nicoleta.plaian@gmail.com>
2022-09-15 15:06:12 +0300

    .
    .

--- DashboardTeam7/Entities/User.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DashboardTeam7.Entities
-{
-    public class User
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { TrainingData, EventSignUp, VirtualAttendance } from "./data";
+
+function App() {
+  return (
+    <div>
+      <h2>{JSON.stringify(TrainingData)}</h2>
+    </div>
+  );
+}
+
+export default App;
